3e0a626b679786cc6fddb5fde3689a4cebae60d2
Maris Klava <maris.klava@gmail.com>
2023-03-01 13:58:17 +0200

    Filtering method, end-user views

--- assets/home.js
+++
@@ -1,9 +0,0 @@
-function Get(uri){
-    var req = new XMLHttpRequest(); // a new request
-    req.open("GET",uri,false);
-    req.send(null);
-    return req.responseText;          
-}
-
-events = Get("/events")
-console.log(events)
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,8 @@ use rocket::http::{Cookie, CookieJar, SameSite};
 use rocket::Request;
 use rocket::fs::FileServer;
 use rocket::response::Redirect;
-use rocket_db_pools::{sqlx, sqlx::Row, sqlx::postgres::*, Database, Connection};
+use rocket_db_pools::{sqlx::{self, Row, postgres::*, query_builder::QueryBuilder, Execute, pool::PoolConnection}, Database, Connection};
+
 use chrono::{DateTime, TimeZone, NaiveDateTime, Utc};
 
 use rocket_oauth2::{OAuth2, TokenResponse};
@@ -14,15 +15,10 @@ struct Google;
 
 use rocket_dyn_templates::{Template, context, tera::Context};
 
-/*extern crate oauth2;
-use oauth2::{basic::{BasicClient, BasicErrorResponseType, BasicTokenType}, revocation::StandardRevocableToken, StandardErrorResponse, StandardTokenResponse, RevocationErrorResponseType, EmptyExtraTokenFields, StandardTokenIntrospectionResponse, /*TokenResponse*/};
-use oauth2::{
-    AuthUrl, AuthorizationCode, ClientId, ClientSecret, CsrfToken, PkceCodeChallenge, RedirectUrl,
-    RevocationUrl, Scope, TokenUrl, IntrospectionUrl
-};*/
-
-use jwksclient2::error::Error;
+use jwksclient2::{error::Error, jwt::Payload};
 use jwksclient2::keyset::KeyStore;
+
+use bevy_reflect::Reflect;
 
 #[derive(Database)]
 #[database("tikifi")]
@@ -41,80 +37,162 @@ pub struct EventSubmission {
     author: i64,
 }
 
-#[derive(FromForm)]
-pub struct VenueSubmission<'r> {
-    name: String,
-    description: String,
-    location: String,
-    thumbnail: rocket::fs::TempFile<'r>,
-}
-
-#[serde(crate = "rocket::serde")]
 #[derive(Debug, Deserialize, Serialize, Clone)]
+#[serde(crate = "rocket::serde")]
 pub struct Event {
     uid: i64,
     name: String,
     description: String,
     event_date: DateTime<Utc>,
-    venue: i64,
-    author: i64,
-}
-
-#[serde(crate = "rocket::serde")]
-#[derive(Debug, Deserialize, Serialize, Clone)]
-pub struct Listing {
-    uid: i64,
-    name: String,
-    event_date: DateTime<Utc>,
     venue_id: i64,
     venue_name: String,
+    thumbnail_url: String,
+    price: i64,
 }
 
-#[serde(crate = "rocket::serde")]
+#[derive(Default, Debug)]
+struct EventFilterCriteria {
+    uid: Option<i64>,
+    venue_name: Option<String>,
+    event_date: Option<DateTime<Utc>>,
+    venue_id: Option<i64>,
+    is_draft: bool,
+    is_active: bool, //UNIMPLEMENTED
+    limit: Option<i32>,
+}
+
+impl EventFilterCriteria {
+    fn new() -> EventFilterCriteria {
+        EventFilterCriteria { ..Default::default() }
+    }
+
+    async fn exec_query(self, mut conn: &mut PoolConnection<Postgres>) -> Result<Vec<Event>, rocket_db_pools::sqlx::Error> {
+        fn sep(c: i32) -> String {
+            if(c==1) { return " WHERE ".to_string() }
+            else { return " AND ".to_string() }
+        }
+
+        let mut q = QueryBuilder::new("SELECT ev.uid, ev.name, ev.description, ev.event_date, ev.price, ev.thumbnail_url, ev.venue AS venue_id, ven.name AS venue_name FROM events AS ev JOIN venues AS ven ON ev.venue = ven.uid");
+        
+        if(self.uid.is_some()) { q.push(" AND ev.uid = "); q.push_bind(self.uid.unwrap());};
+        if(self.venue_id.is_some()) { q.push(" AND ev.venue = "); q.push_bind(self.venue_id.unwrap());};
+        if(self.limit.is_some()) { q.push(" LIMIT "); q.push_bind(self.limit.unwrap());};
+
+        q.build().map(|row: PgRow| Event {
+            uid: row.get("uid"),
+            name: row.get("name"),
+            description: row.get("description"),
+            event_date: row.get("event_date"), //need to figure out datetime formatting
+            venue_id: row.get("venue_id"),
+            venue_name: row.get("venue_name"),
+            thumbnail_url: row.get("thumbnail_url"),
+            price: row.get("price"),
+        }).fetch_all(&mut *conn).await
+    }
+}
+
 #[derive(Debug, Deserialize, Serialize, Clone)]
+#[serde(crate = "rocket::serde")]
 pub struct Venue {
     uid: i64,
     name: String,
     description: String,
     capacity: i64,
     address: String,
+    thumbnail_url: String,
 }
 
-async fn get_current_user(cookies: &CookieJar<'_>) -> Option<String> {
+#[derive(FromForm)]
+pub struct VenueSubmission<> {
+    name: String,
+    description: String,
+    capacity: i64,
+    address: String,
+    thumbnail_url: String,
+}
+//thumbnail: rocket::fs::TempFile<'r>,
+
+#[derive(Default, Debug)]
+struct VenueFilterCriteria {
+    uid: Option<i64>,
+    owner: Option<String>,
+    name: Option<String>,
+    description: Option<String>,
+    capacity: Option<i64>,
+    address: Option<String>,
+}
+
+impl VenueFilterCriteria {
+    fn new() -> VenueFilterCriteria {
+        VenueFilterCriteria { ..Default::default() }
+    }
+    async fn exec_query(self, mut conn: &mut PoolConnection<Postgres>) -> Result<Vec<Venue>, rocket_db_pools::sqlx::Error> {
+        fn sep(c: i32) -> String {
+            if(c==1) { return " WHERE ".to_string() }
+            else { return " AND ".to_string() }
+        }
+
+        let mut q = QueryBuilder::new("SELECT ven.uid, ven.name, ven.description, ven.capacity, ven.address, ven.thumbnail_url FROM venues AS ven WHERE true=true");
+        
+        if(self.uid.is_some()) { q.push(" AND uid = "); q.push_bind(self.uid.unwrap());};
+        if(self.owner.is_some()) { q.push(" AND owner = "); q.push_bind(self.owner.unwrap());};
+
+        q.build().map(|row: PgRow| Venue {
+            uid: row.get("uid"),
+            name: row.get("name"),
+            description: row.get("description"),
+            capacity: row.get("capacity"),
+            address: row.get("address"),
+            thumbnail_url: row.get("thumbnail_url"),
+        }).fetch_all(&mut *conn).await
+
+    }
+}
+
+#[derive(Serialize, Debug)]
+#[serde(crate = "rocket::serde")]
+pub struct UserInfo {
+    id: String,
+    avatar: String,
+}
+
+async fn get_current_user(cookies: &CookieJar<'_>) -> Option<UserInfo> {
     match cookies.get_private("token") { 
         Some(te) => {
-            let jkws_url = "https://www.googleapis.com/oauth2/v3/certs";
-            let key_set = KeyStore::new_from(jkws_url.to_owned()).await.unwrap();
-        
-            match key_set.verify(&te.value().to_string().clone()) {
-                Ok(jwt) => {
-                    return Some(jwt.payload().get_str("sub").expect("Expected sub value").to_owned())
-                }
-                Err(Error { msg, typ: _ }) => {
-                    eprintln!("Could not verify token. Reason: {}", msg);
-                    return None
-                }
-            };
+            return jwks_verify_token(te.value().to_string().clone()).await
         },
         None => return None,
     }
 }
 
+async fn jwks_verify_token(token: String) -> Option<UserInfo> {
+    let jkws_url = "https://www.googleapis.com/oauth2/v3/certs";
+    let key_set = KeyStore::new_from(jkws_url.to_owned()).await.unwrap();
+
+    match key_set.verify(&token) {
+        Ok(jwt) => {
+            let payload = jwt.payload();
+            return Some(
+                UserInfo {
+                    id: payload.get_str("sub").expect("Expected sub value").to_owned(),
+                    avatar: payload.get_str("picture").expect("Expected picture value").to_owned(),
+                }
+            )
+        }
+        Err(Error { msg, typ: _ }) => {
+            eprintln!("Could not verify token. Reason: {}", msg);
+            return None
+        }
+    };
+}
+
 #[get("/login/google")]
 async fn google_login(oauth2: OAuth2<Google>, cookies: &CookieJar<'_>) -> Redirect {
-    
-    /*let (auth_url, csrf_token) = oauth_client().await
-    .authorize_url(CsrfToken::new_random)
-    .add_scope(Scope::new("https://www.googleapis.com/auth/userinfo.profile".to_string()))
-    .url();
-
-    Redirect::to(auth_url.to_string())*/
     oauth2.get_redirect(cookies, &["profile"]).unwrap()
 }
 
 #[get("/auth/google")]
-async fn google_callback(token: TokenResponse<Google>, cookies: &CookieJar<'_>) -> Redirect
-{
+async fn google_callback(mut conn: Connection<Logs>, token: TokenResponse<Google>, cookies: &CookieJar<'_>) -> Redirect {
     let to = token.as_value().get("id_token").expect("invalid ID token");
     let ts = to.as_str().expect("invalid ID token").to_string();
 
@@ -124,12 +202,12 @@ async fn google_callback(token: TokenResponse<Google>, cookies: &CookieJar<'_>) 
             .finish()
     );
 
-    //println!("{}", ts.clone());
-
+    let user_id = jwks_verify_token(ts.clone()).await.expect("Expected valid user").id;
+    sqlx::query("INSERT INTO public.users (uid, role) VALUES ($1, $2) ON CONFLICT DO NOTHING;").bind(user_id).bind(0).execute(&mut *conn).await.expect("Failed to add user");
     Redirect::to("/")
 }
 
-#[post("/events", format = "json", data = "<data>")]
+/*#[post("/events", format = "json", data = "<data>")]
 async fn add_event(data: rocket::serde::json::Json<EventSubmission>, mut conn: Connection<Logs>) -> std::io::Result<String> {
     let a = sqlx::query("INSERT INTO public.events(
         name, description, start_date, end_date, event_date, is_draft, venue, author)
@@ -144,106 +222,184 @@ async fn add_event(data: rocket::serde::json::Json<EventSubmission>, mut conn: C
     .bind(&data.author)
     .execute(&mut *conn).await.ok();
     Ok(format!("Hello!"))
-}
+}*/
 
 #[post("/venues", data = "<data>")]
-async fn add_venue(data: rocket::form::Form<VenueSubmission<'_>>, mut conn: Connection<Logs>) -> std::io::Result<String> {
-    let a = sqlx::query("INSERT INTO public.venues(
-        name, description, address, capacity)
-        VALUES ($1, $2, $3, 999)")
-    .bind(&data.name)
-    .bind(&data.description)
-    .bind(&data.location)
-    .execute(&mut *conn).await.ok();
-    Ok(format!("Hello!"))
+async fn add_venue(data: rocket::form::Form<VenueSubmission<>>, mut conn: Connection<Logs>, cookies: &CookieJar<'_>) -> Option<String> {
+    match get_current_user(cookies).await {
+        Some(c) => {
+            let a = sqlx::query("INSERT INTO public.venues(
+                name, description, capacity, address, thumbnail_url, owner)
+                VALUES ($1, $2, $3, $4, $5)")
+            .bind(&data.name)
+            .bind(&data.description)
+            .bind(&data.capacity)
+            .bind(&data.address)
+            .bind(&data.thumbnail_url)
+            .bind(&c.id)
+            .execute(&mut *conn).await.ok()?;
+            Some(format!("Hello!"))
+        },
+        None => None
+    }
 }
 
-#[get("/events")]
-async fn get_active_events(mut conn: Connection<Logs>) -> Option<String> {
-    let events: Vec<Event> = sqlx::query("SELECT uid, name, description, event_date, venue, author FROM events WHERE NOW() BETWEEN start_date AND end_date AND is_draft=false")
-        .map(|row: PgRow| Event {
-            uid: row.get("uid"),
-            name: row.get("name"),
-            description: row.get("description"), 
-            event_date: row.get("event_date"),
-            venue: row.get("venue"),
-            author: row.get("author"),
-        })
-        .fetch_all(&mut *conn).await.ok()?;
-    Some(rocket::serde::json::to_string(&events).ok()?)
-}//In case of none replace with something else please
-
-async fn get_event_listings(mut conn: Connection<Logs>) -> Option<Vec<Listing>> {
-    Some(sqlx::query("SELECT e.uid, e.name, e.event_date, e.venue AS venue_id, v.name AS venue_name FROM events AS e JOIN venues AS v ON e.venue = v.uid WHERE NOW() BETWEEN start_date AND end_date AND e.is_draft=false")
-        .map(|row: PgRow| Listing {
-            uid: row.get("uid"),
-            name: row.get("name"),
-            event_date: row.get("event_date"), //need to figure out datetime formatting
-            venue_id: row.get("venue_id"),
-            venue_name: row.get("venue_name"),
-        })
-        .fetch_all(&mut *conn).await.ok()?)
+async fn get_featured_events(mut conn: &mut PoolConnection<Postgres>, limit: i32) -> Option<Vec<Event>> {
+    let mut criteria = EventFilterCriteria::new();
+    criteria.limit = Some(1); 
+    criteria.exec_query(&mut *conn).await.ok()
 }
 
-async fn get_events(mut conn: Connection<Logs>) -> Option<Vec<Listing>> {
-    Some(sqlx::query("SELECT e.uid, e.name, e.event_date, e.venue AS venue_id, v.name AS venue_name FROM events AS e JOIN venues AS v ON e.venue = v.uid WHERE NOW() BETWEEN start_date AND end_date AND e.is_draft=false")
-        .map(|row: PgRow| Listing {
-            uid: row.get("uid"),
-            name: row.get("name"),
-            event_date: row.get("event_date"), //need to figure out datetime formatting
-            venue_id: row.get("venue_id"),
-            venue_name: row.get("venue_name"),
-        })
-        .fetch_all(&mut *conn).await.ok()?)
+#[get("/events/<id>")]
+async fn get_event(mut conn: Connection<Logs>, id: i64, cookies: &CookieJar<'_>) -> Option<Template> {
+    let mut ctx = Context::new();
+
+    match get_current_user(cookies).await {
+        Some(c) => ctx.insert("user", &c),
+        None => ctx.insert("user", &false),
+    };
+    
+    let mut criteria = EventFilterCriteria::new();
+    criteria.uid = Some(id); 
+    let event = criteria.exec_query(&mut *conn).await.ok()?;
+    if(event.len() == 0) { return None }
+    ctx.insert("event", &event[0]);
+
+    Some(Template::render("event", ctx.into_json()))
 }
 
 #[get("/venues/<id>")]
-async fn get_venue(mut conn: Connection<Logs>, id: i64) -> Option<Template> {
-    let venue: Venue = sqlx::query("SELECT uid, name, description, capacity, address FROM venues WHERE uid = $1").bind(id)
+async fn get_venue(mut conn: Connection<Logs>, id: i64, cookies: &CookieJar<'_>) -> Option<Template> {
+    let mut ctx = Context::new();
+
+    match get_current_user(cookies).await {
+        Some(c) => ctx.insert("user", &c),
+        None => ctx.insert("user", &false),
+    };
+    
+    let venue: Venue = sqlx::query("SELECT uid, name, description, capacity, address, thumbnail_url FROM venues WHERE uid = $1").bind(id)
         .map(|row: PgRow| Venue {
             uid: row.get("uid"),
             name: row.get("name"),
-            description: row.get("description"), //need to figure out datetime formatting
+            description: row.get("description"),
             capacity: row.get("capacity"),
             address: row.get("address"),
+            thumbnail_url: row.get("thumbnail_url"),
         })
         .fetch_one(&mut *conn).await.ok()?;
-    Some(Template::render("venue", context! {
-        venue: venue,
-    }))
+    ctx.insert("venue", &venue);
+
+    let mut criteria = EventFilterCriteria::new();
+    criteria.venue_id = Some(id); 
+    let events = criteria.exec_query(&mut *conn).await.ok();
+
+    match events {
+        Some(e) => {
+            ctx.insert("events", &e);
+        }
+        None => {
+            ctx.insert("events", &false);
+        }
+    }
+
+    Some(Template::render("venue", ctx.into_json()))
+    
 }
 
 #[get("/")]
 async fn index(mut conn: Connection<Logs>, cookies: &CookieJar<'_>) -> Option<Template> {
-    println!("{:?}",get_current_user(cookies).await);
-    let events: Vec<Listing> = get_event_listings(conn).await?; 
     let mut ctx = Context::new();
-    ctx.insert("events", &events);
-    ctx.insert("logged_in", &true); //?
-    match cookies.get("token") { //?
-        Some(c) => ctx.insert("token", &c.to_string()), //?
-        None => {} //?
-    }; //?
+
+    match get_current_user(cookies).await {
+        Some(c) => ctx.insert("user", &c),
+        None => ctx.insert("user", &false),
+    };
+
+    let mut criteria = EventFilterCriteria::new();
+    criteria.is_active = true; // UNIMPLEMENTED
+    let events = criteria.exec_query(&mut *conn).await.ok();
+
+    match events {
+        Some(e) => {
+            ctx.insert("events", &e);
+        }
+        None => {
+            ctx.insert("events", &false);
+        }
+    }
+
+    let mut criteria = VenueFilterCriteria::new();
+    //criteria.limit = 5; // UNIMPLEMENTED
+    let venues = criteria.exec_query(&mut *conn).await.ok();
+
+    match venues {
+        Some(e) => {
+            ctx.insert("venues", &e);
+        }
+        None => {
+            ctx.insert("venues", &false);
+        }
+    }
+
+    let featured = get_featured_events(&mut *conn, 1).await;
+
+    match featured {
+        Some(e) => {
+            ctx.insert("featured", &e);
+        }
+        None => {
+            ctx.insert("featured", &false);
+        }
+    }
+
     Some(Template::render("index", ctx.into_json()))
+}
+
+#[get("/dashboard")]
+async fn dashboard() -> Redirect {
+    Redirect::to("/dashboard/venues")
 }
 
 #[get("/dashboard/venues")]
 async fn dashboard_venues(mut conn: Connection<Logs>, cookies: &CookieJar<'_>) -> Option<Template> {
-    Some(Template::render("dashboard_venues", context! {
+    let mut ctx = Context::new();
+    let mut criteria = VenueFilterCriteria::new();
 
-    }))
+    match get_current_user(cookies).await {
+        Some(c) => {
+            ctx.insert("user", &c);
+            criteria.owner = Some(c.id.clone());
+        },
+        None => {Redirect::to("/");},
+    };
+    print!("{:?}", criteria);
+
+    let venues = criteria.exec_query(&mut *conn).await.ok(); 
+
+    match venues {
+        Some(e) => {
+            ctx.insert("venues", &e);
+        }
+        None => {
+            ctx.insert("venues", &false);
+        }
+    }
+
+    Some(Template::render("dashboard_venues", ctx.into_json()))
 }
 
 #[get("/dashboard/events")]
 async fn dashboard_events(mut conn: Connection<Logs>, cookies: &CookieJar<'_>) -> Option<Template> {
-    let events: Vec<Listing> = get_event_listings(conn).await?; 
     let mut ctx = Context::new();
-    ctx.insert("events", &events);
-    ctx.insert("logged_in", &true); //?
-    match cookies.get("token") { //?
-        Some(c) => ctx.insert("token", &c.to_string()), //?
-        None => {} //?
-    }; //?
+    
+    match get_current_user(cookies).await {
+        Some(c) => ctx.insert("user", &c),
+        None => {Redirect::to("/");},
+    };
+    
+    //let events: Vec<Event> = get_event_listings(conn).await?; 
+    //ctx.insert("events", &events);
+
     Some(Template::render("dashboard_events", ctx.into_json()))
 }
 
@@ -251,7 +407,7 @@ async fn dashboard_events(mut conn: Connection<Logs>, cookies: &CookieJar<'_>) -
 fn rocket() -> _ {
     rocket::build()
         .attach(Logs::init())
-        .mount("/", routes![get_active_events, google_callback, google_login, index, dashboard_venues, dashboard_events, get_venue, add_event, add_venue])
+        .mount("/", routes![google_callback, google_login, index, dashboard_venues, dashboard_events, get_venue, get_event, /*add_event,*/ add_venue, dashboard])
         .mount("/assets", FileServer::from("./assets"))
         .attach(OAuth2::<Google>::fairing("google"))
         .attach(Template::fairing())
